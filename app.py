ff5ffe5f9bcbeb4c778ff6faf7d097ea5e747b0e
Demotive <mat@demotive.com>
2014-04-11 14:36:08 +0100

    Set up skeleton

--- app.py
+++ app.py
@@ -119,5 +119,9 @@ def map():
 def sell_find_property():
     return render_template('sell_find_property.html')
 
+@app.route('/sell/enter-details')
+def sell_find_property():
+    return render_template('sell_enter_details.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8001)
