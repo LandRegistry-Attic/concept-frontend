2af85c6264f62c8affdeb5b28dd1efee4cb0d2a1
Gemma Leigh <hello@gemmaleigh.com>
2014-04-29 11:32:38 +0100

    Update front end styles and freeze templates
    - Update GOV.UK elements styles
    - Fix grid layout
    - Move old files into archive for now as reference
    - New freeze section pages
    - Update "freeze register" to "protect an interest"

---
+++ static/javascripts/elements.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+  // Chunky labels for radios & checkboxes
+
+  if ($('form').length>0) {
+
+    $(".block-label").each(function() {
+
+      // Add focus
+      $(".block-label input").focus(function() {
+        $("label[for='" + this.id + "']").addClass("add-focus");
+        }).blur(function() {
+        $("label").removeClass("add-focus");
+      });
+
+      // Add selected class
+      $('input:checked').parent().addClass('selected');
+
+    });
+
+    // Add/remove selected class
+    $('.block-label').find('input[type=radio], input[type=checkbox]').click(function() {
+
+      $('input:not(:checked)').parent().removeClass('selected');
+      $('input:checked').parent().addClass('selected');
+
+      $('.toggle-content').hide();
+
+      var target = $('input:checked').parent().attr('data-target');
+      $('#'+target).show();
+
+    });
+
+    // For pre-checked inputs, show toggled content
+    var target = $('input:checked').parent().attr('data-target');
+    $('#'+target).show();
+
+    }
+
+});
